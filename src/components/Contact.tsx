a4f7b42f65a5c1a48fa5d96fba2bcdd859230f69
Chandaluri Pavan Durga Sai Gutpa <2200032476@kluniversity.in>
2025-06-14 12:35:38 +0530

    Update Contact.tsx

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -26,8 +26,8 @@ const Contact = () => {
     {
       icon: Linkedin,
       label: 'LinkedIn',
-      value: 'linkedin.com/in/ch-pavan-70b47536a/',
-      href: 'https://www.linkedin.com/in/ch-pavan-70b47536a/',
+      value: 'linkedin.com/in/chandaluri-pavan-durga-s-325374254/',
+      href: 'https://www.linkedin.com/in/chandaluri-pavan-durga-s-325374254/',
       color: 'cyber-red'
     }
   ];
